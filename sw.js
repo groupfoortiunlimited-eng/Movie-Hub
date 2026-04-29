ff0bd65d5a7c15c5ef8384b0220ca9431aed0318
groupfoortiunlimited-eng <groupfoortiunlimited@gmail.com>
2026-04-29 14:38:57 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,19 @@
-const CACHE_NAME = 'moviehub-v2';
+const CACHE_NAME = 'moviehub-v3';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo.jpg'
+  '/',
+  '/index.html',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
